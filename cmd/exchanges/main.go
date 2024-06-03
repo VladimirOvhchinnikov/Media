27debe1bb286a2d4c8548b8a96aac2c71647db1d
OvchinnikovVI <ovchinnikovvi@hotmail.com>
2024-06-03 12:46:09 +0300

    main clear

--- cmd/exchanges/main.go
+++ cmd/exchanges/main.go
@@ -24,27 +24,19 @@ func main() {
 	var router *routing.GoChiRouter = routing.NewGoChiRouting(logger, *handler)
 	var server serve.Server = serve.NewServerHTTP(logger, ":8080", router)
 
-	if err := server.Start(); err != nil {
-		os.Exit(1)
-	}
-
-	// Запуск сервера в отдельной горутине
 	go func() {
 		if err := server.Start(); err != nil {
 			logger.Fatalf("Error starting the server: %s", err)
 		}
 	}()
 
-	// Создание канала для прослушивания сигналов системы
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
 
-	// Блокировка до получения сигнала
 	<-stop
 
 	logger.Info("Shutting down the server...")
 
-	// Создание контекста с тайм-аутом для graceful shutdown
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
